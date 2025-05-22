7695a7896238e352ca6d12f8db58643da8064071
junyonglee0223 <ljyong3339@gmail.com>
2025-05-22 17:00:34 +0900

    refactoring: move service, repository to order package

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -1,5 +1,7 @@
 package hello.aop;
 
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
